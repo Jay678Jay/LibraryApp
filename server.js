d9b189ec5337c7df375281609fca03d064c420f5
Jongwon Lee <jlee678@my.centennialcollege.ca>
2025-07-26 13:32:13 -0400

    Update for deployment

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 // This file sets up the Express server, connects to MongoDB using environment variables,
 // and defines the main routes for the application.
 
+import { path } from path;
+import { fileURLToPath } from 'url';
+
+//Create _dirname equivalent for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
@@ -37,9 +44,17 @@ app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => {
   res.send('Library App API is running...');
 });
-
+app.use(express.static)
 // Use the PORT from .env or default to 5000
 const PORT = process.env.PORT || 5000;
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+  app.get(/^\/(?!api).*$/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+});
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
